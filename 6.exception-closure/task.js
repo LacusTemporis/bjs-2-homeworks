e588f33a605d6829b7a50da723105e1c274a0661
Dialuna <evashustova@gmail.com>
2023-04-11 17:39:13 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,55 @@
-﻿
+﻿function parseCount(value) {
+	let parse = Number.parseFloat(value);
+	if (isNaN(parse)) {
+		throw new Error("Невалидное значение")
+	} else {
+		return parse;
+	}
+}
+
+function validateCount(value) {
+	try {
+		return parseCount(value);
+	} catch (error) {
+		return error
+	}
+}
+
+
+class Triangle {
+	constructor(a, b, c) {
+		if (a + b <= c || a + c <= b || b + c <= a) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+
+		this.a = a;
+		this.b = b;
+		this.c = c;
+
+	}
+
+	get perimeter() {
+		return this._perimeter = this.a + this.b + this.c;
+	}
+
+	get area() {
+		const p = this._perimeter / 2;
+		this._area = Number.parseFloat(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+		return this._area;
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c)
+	} catch (error) {
+		return {
+			get perimeter() {
+				return "Ошибка! Треугольник не существует"
+			},
+			get area() {
+				return "Ошибка! Треугольник не существует"
+			}
+		}
+	}
+}
